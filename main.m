6cd3388e27f2cbc9ef718ebaf95c6d2d4b71dab2
Daniel Chou Rainho <choudaniel3@gmail.com>
2024-12-18 14:19:24 +0100

    bug fix

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 figure('Units', 'pixels', 'Position', [100 100 560 420]);
 axes('Units', 'normalized');
-colorbar;
+c = colorbar;
 disp('Axes Position:')
 disp(get(gca,'Position'))
 disp('Colorbar Position:')
