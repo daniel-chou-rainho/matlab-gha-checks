a4ca23b5d3e6c70cf148fc7c0c11fbf156337c94
Daniel Chou Rainho <choudaniel3@gmail.com>
2024-12-18 14:23:28 +0100

    set font name and colorbar location to eastoutside

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 figure('Units', 'pixels', 'Position', [100 100 560 420]);
 axes('Units', 'normalized');
-c = colorbar;
+c = colorbar('Location', 'eastoutside');
+set(c, 'FontName', 'Arial');
 disp('Axes Position:')
 disp(get(gca,'Position'))
 disp('Colorbar Position:')
