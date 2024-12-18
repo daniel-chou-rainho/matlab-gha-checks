cb891fcc668ba08ea0eca04a10dff3a56773002e
Daniel Chou Rainho <choudaniel3@gmail.com>
2024-12-18 14:03:50 +0100

    colorbar

--- main.m
+++ main.m
@@ -2,7 +2,10 @@ figure('Units', 'pixels', 'Position', [100 100 560 420]);
 ax = axes('Units', 'normalized');
 x = rand(10);
 surf(x)
+c = colorbar;
 disp('Screen DPI:')
 disp(get(0,'ScreenPixelsPerInch'))
 disp('Axes Position:')
 disp(get(gca,'Position'))
+disp('Colorbar Position:')
+disp(get(c,'Position'))
