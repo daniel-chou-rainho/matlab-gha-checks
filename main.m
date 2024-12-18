7ca9dc96e26b6b83816f590132cb381e72233d77
Daniel Chou Rainho <choudaniel3@gmail.com>
2024-12-18 13:57:09 +0100

    just creating axes

--- main.m
+++ main.m
@@ -1,19 +1,8 @@
-figure('Units', 'pixels', 'Position', [100 100 560 420], 'Renderer', 'painters');
+figure('Units', 'pixels', 'Position', [100 100 560 420]);
 ax = axes('Units', 'normalized');
-[X,Y] = meshgrid(-2:0.25:2);
-Z = X.*exp(-X.^2 - Y.^2);
-surf(X,Y,Z)
-c = colorbar;
-c.Label.String = 'Temperature (K)';
-c.Label.FontName = 'Arial';
-c.Label.FontSize = 12;
-
+x = rand(10);
+surf(x)
 disp('Screen DPI:')
 disp(get(0,'ScreenPixelsPerInch'))
-
 disp('Axes Position:')
 disp(get(gca,'Position'))
-disp('Colorbar Position:')
-disp(get(c,'Position'))
-disp('Colorbar Label Extent:')
-disp(get(c.Label,'Extent'))
