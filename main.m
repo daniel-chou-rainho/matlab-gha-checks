2ced62b48f358fd2f58291e1043ec43113475d32
Daniel Chou Rainho <choudaniel3@gmail.com>
2024-12-18 14:35:04 +0100

    more logs

--- main.m
+++ main.m
@@ -1,8 +1,13 @@
 figure('Units', 'pixels', 'Position', [100 100 560 420]);
 axes('Units', 'normalized');
-c = colorbar('Location', 'eastoutside');
-set(c, 'FontName', 'Arial');
-disp('Axes Position:')
-disp(get(gca,'Position'))
-disp('Colorbar Position:')
-disp(get(c,'Position'))
+c = colorbar;
+disp('MATLAB Version:')
+disp(version)
+disp('Screen DPI:')
+disp(get(0,'ScreenPixelsPerInch'))
+disp('Screen Size:')
+disp(get(0,'ScreenSize'))
+disp('Default Font:')
+disp(get(0,'DefaultFigureDefaultTextFontName'))
+disp('Graphics Renderer:')
+disp(get(gcf,'Renderer'))
