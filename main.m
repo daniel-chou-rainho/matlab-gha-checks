29b5516183eda5d358cd991ca45dda08d97bd14e
Daniel Chou Rainho <choudaniel3@gmail.com>
2024-12-18 11:41:57 +0100

    painters

--- main.m
+++ main.m
@@ -1,23 +1,16 @@
-figure('Units', 'pixels', 'Position', [100 100 560 420]);
+figure('Units', 'pixels', 'Position', [100 100 560 420], 'Renderer', 'painters');
 ax = axes('Units', 'normalized');
-
 [X,Y] = meshgrid(-2:0.25:2);
 Z = X.*exp(-X.^2 - Y.^2);
 surf(X,Y,Z)
-
 c = colorbar;
 c.Label.String = 'Temperature (K)';
 c.Label.FontName = 'Arial';
 c.Label.FontSize = 12;
 
-% Get and display properties
-disp('Figure Properties:')
-disp(get(gcf))
-
-disp('Axes Properties:')
-disp(get(gca))
-
-disp('Colorbar Properties:')
-disp(get(c))
-disp('Colorbar Label Properties:')
-disp(get(c.Label))
+disp('Axes Position:')
+disp(get(gca,'Position'))
+disp('Colorbar Position:')
+disp(get(c,'Position'))
+disp('Colorbar Label Extent:')
+disp(get(c.Label,'Extent'))
