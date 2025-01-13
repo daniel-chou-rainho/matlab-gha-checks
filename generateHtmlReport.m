b535fa3cfc785ccec196324fee67c631d2407d9c
Daniel Chou Rainho <choudaniel3@gmail.com>
2025-01-13 14:48:10 +0100

    create svg on windows gha

--- generateHtmlReport.m
+++ generateHtmlReport.m
@@ -1,26 +1,35 @@
 function generateHtmlReport(differencesPath)
-   data = load(differencesPath);
-   differences = data.allDifferences.differences;
-   maxLength = 50;
-   
-   header = ['<table>\n<tr>' ...
-             '<th>Class</th><th>Parent</th><th>Field</th>' ...
+    data = load(differencesPath);
+    differences = data.allDifferences.differences;
+    maxLength = 50;
+    
+    % Get system info
+    currentTime = datestr(now);
+    screenSize = get(0, 'ScreenSize');
+    screenDPI = get(0, 'ScreenPixelsPerInch');
+    
+    % Create header with system info
+    header = ['<table>\n' ...
+             sprintf('<tr><td colspan="6">Generated: %s</td></tr>\n', currentTime) ...
+             sprintf('<tr><td colspan="6">Screen Size: %dx%d</td></tr>\n', screenSize(3), screenSize(4)) ...
+             sprintf('<tr><td colspan="6">Screen DPI: %.0f</td></tr>\n', screenDPI) ...
+             '<tr><th>Class</th><th>Parent</th><th>Field</th>' ...
              '<th>Baseline</th><th>Test</th><th>Matches</th></tr>'];
              
-   rows = strings(length(differences), 1);
-   for i = 1:length(differences)
-       diff = differences(i);
-       rows(i) = sprintf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>', ...
-           diff.class, diff.parent, diff.field, ...
-           convertToString(diff.baseline, maxLength), ...
-           convertToString(diff.test, maxLength), ...
-           string(diff.matches));
-   end
-   
-   fullText = strjoin([header; rows; "</table>"], newline);
-   fid = fopen('output/differences.html', 'w');
-   fprintf(fid, '%s', char(fullText));
-   fclose(fid);
+    rows = strings(length(differences), 1);
+    for i = 1:length(differences)
+        diff = differences(i);
+        rows(i) = sprintf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>', ...
+            diff.class, diff.parent, diff.field, ...
+            convertToString(diff.baseline, maxLength), ...
+            convertToString(diff.test, maxLength), ...
+            string(diff.matches));
+    end
+    
+    fullText = strjoin([header; rows; "</table>"], newline);
+    fid = fopen('output/differences.html', 'w');
+    fprintf(fid, '%s', char(fullText));
+    fclose(fid);
 end
 
 function str = convertToString(val, maxLength)
@@ -35,7 +44,7 @@ function str = convertToString(val, maxLength)
     else
         str = '<complex value>';
     end
-    
+
     if length(str) > maxLength
         str = [str(1:maxLength-3) '...'];
     end
--- test.m
+++ test.m
@@ -1,6 +1,5 @@
-current_dpi = get(0, 'ScreenPixelsPerInch');
-disp(['Current DPI: ', num2str(current_dpi)]);
-
-set(0, 'ScreenPixelsPerInch', 150);
-new_dpi = get(0, 'ScreenPixelsPerInch');
-disp(['New DPI: ', num2str(new_dpi)]);
+x = peaks(100);
+figure
+imagesc(x)
+colorbar
+saveas(gcf,'figure.svg')
